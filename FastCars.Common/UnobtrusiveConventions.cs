57e2f977c2c1606a66c5c99d7a21308aff9de8ec
Dawson Kroeker <dkroeker@gmail.com>
2014-09-10 21:24:22 -0500

    Ex3 Task2
    Define a common unobtrusive convention to be used for all endpoints

---
+++ FastCars.Common/UnobtrusiveConventions.cs
@@ -0,0 +1,14 @@
+﻿using NServiceBus;
+
+namespace FastCars.Common
+{
+    public class UnobtrusiveConventions : IWantToRunBeforeConfiguration
+    {
+        public void Init()
+        {
+            Configure.Instance.DefiningEventsAs(t => t.Namespace != null
+                                                     && t.Namespace.StartsWith("FastCars")
+                                                     && t.Namespace.EndsWith("Events"));
+        }
+    }
+}
