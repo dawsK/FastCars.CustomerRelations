5be4b2705074953e1f902dc552d285c76585a609
Dawson Kroeker <dkroeker@gmail.com>
2014-09-10 20:33:46 -0500

    Task 2 Create a publisher endpoint (FastCars.CustomerRelations) and configure to MSMQ as the message transport

--- FastCars.CustomerRelations/Bootstrapper.cs
+++ FastCars.CustomerRelations/Bootstrapper.cs
@@ -1,15 +1,23 @@
 ﻿using System;
+using FastCars.Events;
+using NServiceBus;
 
 namespace FastCars.CustomerRelations
 {
-    public class Bootstrapper
+    public class Bootstrapper : IWantToRunWhenBusStartsAndStops
     {
+        public IBus Bus { get; set; }
 
         public void Start()
         {
             Console.WriteLine("Press Enter to publish an event");
             while (Console.ReadLine() != null)
             {
+                Bus.Publish<ClientBecamePreferred>(m =>
+                {
+                    m.ClientId = Guid.NewGuid();
+                    m.PreferredUntil = DateTime.Today.AddDays(30);
+                });
                 Console.WriteLine("Published ClientBecamePreferred event");
             }
         }
